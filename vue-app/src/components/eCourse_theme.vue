46026dc76669d633ffa5fc54eefde93157eb3531
TheKlepo75 <antonio.klepo1@gmail.com>
2022-02-27 18:22:24 +0100

    commit

--- vue-app/src/components/eCourse_theme.vue
+++ vue-app/src/components/eCourse_theme.vue
@@ -4,22 +4,45 @@
             <h1>Laboratorijske vježbe</h1>
         </div>
         <div>
-            theme_child
+            <ThemeChild class="themeChild" />
         </div>
     </section>
 </template>
 
 <script>
+
+import ThemeChild from './theme_child.vue'
+
 export default {
-    
+    components: {
+        ThemeChild
+    }
 }
 </script>
 
 <style scoped>
 
 section {
-    outline: 1px solid red;
-    margin-top: 80px;
+    margin-top: 46px;
+    width: 50vw;
+}
+.container {
+    padding-bottom: 8px;
+    border-bottom: 2px solid rgb(255, 255, 255, 0.2);
+}
+h1{
+    font-family: Poppins;
+    font-style: normal;
+    font-weight: 200;
+    font-size: 35px;
+    line-height: 54px;
+
+    color: #FFFFFF;
+
+    opacity: 0.8;
+}
+.themeChild{
+    margin-top: 30px;
 }
 
 </style>
---
+++ vue-app/src/components/theme_child.vue
@@ -0,0 +1,28 @@
+<template>
+    <div>
+        <section v-if="themeType === 'U'">
+            Pekmez
+        </section>
+        <section v-else>
+            
+        </section>
+    </div>
+</template>
+
+<script>
+export default {
+    data(){
+        return {
+            themeType: 'U'
+        }
+    }
+}
+</script>
+
+<style scoped>
+
+section:nth-child(1) {
+    outline: 1px solid red;
+}
+
+</style>
