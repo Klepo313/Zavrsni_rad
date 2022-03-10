a116307c9f96476031b4498e699f2798ac7dc6be
TheKlepo75 <antonio.klepo1@gmail.com>
2022-03-10 19:50:02 +0100

    commit

--- server/app.js
+++ server/app.js
@@ -1,15 +1,25 @@
-var express=require('express');
-var cors=require('cors');
-var app=express();
-const client=require('./dbConnect');
-app.use(cors());
+
 require('dotenv').config(); 
 
-app.set("port",process.env.PORT || 3000);
-var port=app.get('port');
+var express = require('express');
+var cors = require('cors');
+var app = express();
+const pool = require('./dbConnect');
+
+const port = 3000;
+
+app.use(cors());
+
+// app.set("port", process.env.PORT || 3000);
+// var port = app.get('port');
+
+app.get("/mjesta", pool.getMjesta)
+app.get("/mjesta/:id", pool.getMjesto)
+app.get("/login/:email/:password", pool.loginUser)
+app.get("/userDetails/:id", pool.getUserDetails)
 
 
-app.listen(port, function(){
-    var host="localhost";
-    console.log("Server listening on port http://%s:%s", host, port);
+app.listen(port, () => {
+    var host = "localhost";
+    console.log(`Server listening on port http://%s:%s`, host, port);
 })
--- server/dbConnect.js
+++ server/dbConnect.js
@@ -1,18 +1,129 @@
-var pg=require('pg');
-var connectionString="postgres://qulaynxc:zwIMATnDC4p_yujWhgC1ccg1x6XpgRab@abul.db.elephantsql.com/qulaynxc";
-var client =new pg.Client(connectionString);
+var pg = require('pg');
+var connectionString = "postgres://qulaynxc:zwIMATnDC4p_yujWhgC1ccg1x6XpgRab@abul.db.elephantsql.com/qulaynxc";
+var pool = new pg.Client(connectionString);
 
-client.connect(function(err){
-    if(err){
-        return console.error('Could not connect to postgres',err); 
-    }
-    client.query('SELECT NOW() AS "theTime" ',function(err,result){
-        
-        if(err) console.error('Error running query',err);
+pool.connect()
 
-        console.log('Db connected:',result.rows[0].theTime);
+const getMjesta = (req, res) => {
+    pool.query("SELECT * FROM mjesta LIMIT 10", (err, results) => {
+        if (err){
+            console.log(err)
+        }
+        res.json(results.rows)
+    })
+}
+
+const getMjesto = async (req, res) => {
+    const id = parseInt(req.params.id)
+
+    pool.query("SELECT mje_naziv FROM mjesta WHERE mje_id = $1", [id], (err, results) => {
+        if (err){
+            console.log(err)
+        } 
+        res.json(results.rows)
+    })
+}
+
+const loginUser = (request, response) => {
+    var email = request.params.email;
+    var passwd = request.params.password; 
     
-    });
-});
+    if(email && passwd){
+        console.log("Pridani parametri: ")
+        console.log("EMAIL: " + email + "\tPASSWORD: " + passwd)
 
-module.exports =client;
+       /* pool.query(`SELECT kor_id, kor_username FROM korisnik 
+                    WHERE kor_username = '${email}' 
+                    AND kor_password = '${passwd}'`, (err, results) => {
+
+                        if(err) throw err; //U slučaju greške
+
+                        else if(typeof(results.rows[0])!=="undefined"){
+
+                            var id = results.rows[0].kor_id
+                            var kor_username = results.rows[0].kor_username
+
+                            console.log("\nPosli upita:")
+                            console.log("ID: " + id)
+                            console.log("EMAIL: " + kor_username);
+
+                            response.status(200).json(results.rows);
+
+                        } else{
+                            console.log("Greška 2")
+                            response.status(200).json({"status":"unsuccessful"}); 
+                        }
+
+                    }
+                )*/
+    
+        pool.query(`SELECT kor_id, kor_username, kor_password,
+                        (SELECT uv.osa_id
+                        FROM   ucenici_v uv
+                        WHERE  uv.osa_kor_id = kor_id) ucenik,
+                        (SELECT pv.osa_id
+                        FROM   profesori_v pv
+                        WHERE  pv.osa_kor_id = kor_id) profesor
+                    FROM   korisnik k
+                    WHERE  kor_username = '${email}'
+                    AND kor_password = '${passwd}' `, (err, results) => {
+
+            if(err) throw err; //U slučaju greške
+
+            else if(typeof(results.rows[0])!=="undefined"){
+
+                var kor_id = results.rows[0].kor_id
+                var kor_username = results.rows[0].kor_username
+                var kor_password = results.rows[0].kor_password
+                var kor_ucenik = results.rows[0].ucenik
+                var kor_prof = results.rows[0].profesor
+
+                //response.status(200).json(results.rows);
+                response.status(200).json({
+                    "status": "successful", 
+                    "osa_id": kor_id, 
+                    "osa_username": kor_username, 
+                    "osa_password": kor_password, 
+                    "osa_ucenik": kor_ucenik,
+                    "osa_profesor": kor_prof
+                })
+
+            } else{
+                console.log("Greška 2")
+                response.status(200).json({"status":"unsuccessful"}); 
+            }
+
+                }
+            )
+    
+    } else{
+        console.log("Greška 1")
+        response.status(200).json({"status":"unsuccessful"}); 
+    }
+
+}
+
+const getUserDetails = (req, res) => {
+    var kor_id = req.params.id;
+
+    if(kor_id){
+        pool.query(`SELECT * FROM osoba WHERE osa_kor_id = ${kor_id}`, (err, results) => {
+            if(err) throw err;
+            else{
+                console.log(results.rows)
+                res.status(200).json(results.rows); 
+            }
+        })
+    } else{
+        console.log("Greška 2")
+        response.status(200).json({"status":"unsuccessful"}); 
+    }
+}
+
+module.exports = {
+    pool,
+    getMjesta,
+    getMjesto,
+    loginUser,
+    getUserDetails,
+} 
